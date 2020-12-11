43eab2b6b8c7f06f4419d33e52924b9f08faf15d
Varshit Dusad <dusadv@gmail.com>
2020-12-11 10:27:53 +0530

    Modified routes.db

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   resources :entries
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  roo to: "entries#index"
+  roo to: "entries#{index}"
 end
